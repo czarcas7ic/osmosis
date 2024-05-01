5208cf70f585d2230c60939d68efe455afca6cfd
Adam Tucker <adamleetucker@outlook.com>
2024-04-30 21:01:29 -0600

    try seq curl

--- scripts/join_and_profile.sh
+++ scripts/join_and_profile.sh
@@ -213,8 +213,8 @@ if [ "$profile_type" == "head" ]; then
     echo "Block height has changed. Success."
 
     # Curl the CPU and heap endpoints simultaneously and store the profiles
-    curl -X GET localhost:6060/debug/pprof/profile?seconds=60 > cpu.prof &
-    curl -X GET localhost:6060/debug/pprof/heap?seconds=60 > heap.prof &
+    curl -X GET localhost:6060/debug/pprof/profile?seconds=60 > cpu.prof
+    curl -X GET localhost:6060/debug/pprof/heap?seconds=60 > heap.prof
 
     # Wait for both curl commands to finish
     wait
